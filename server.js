2a00087eac54b3a76bb73eea99aaf0bada3e27a8
JonasStattin <jonas.i.stattin@gmail.com>
2015-01-29 17:31:44 +0100

    Moved data to database

--- server.js
+++ server.js
@@ -2,38 +2,35 @@ var
 	express = require('express'),
 	http = require('http'),
 	path = require('path'),
-	config = require('./config');
+	config = require('./config')
+
+	mongo = require('mongodb'),
+	monk = require('monk');
 
 var
 	app = express(),
-	server = http.createServer(app);
+	server = http.createServer(app)
+	db = monk('localhost:27017/randoom');
 
 app.set('port', process.env.PORT || config.expressPort);
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Make our db accessible to our router
+app.use(function(req, res, next){
+	req.db = db;
+	next();
+});
 
 server.listen(app.get('port'), function() {
 	console.log('Server listening on port ' + app.get('port'));
 });
 
 app.get('/tables', function(req, res) {
-	var TABLES = [
-		{
-			title: 'My first table',
-			entries: [
-				'The first result',
-				'Result the second',
-				'A third result'
-			]
-		},
-		{
-			title: 'My second table',
-			entries: [
-				'The result premiere',
-				'Secondi Resulti',
-				'Tripoli',
-				'Quattro Formaggio'
-			]
-		}
-	];
-	res.send(JSON.stringify(TABLES));
+
+	var db = req.db;
+	var collection = db.get('tablecollection');
+	collection.find({}, {}, function(e, docs) {
+		res.send(JSON.stringify(docs));
+	});
+
 });
