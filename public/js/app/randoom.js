a2bef450737779a64984ab82401d0296ca1b3ea2
JonasStattin <jonas.i.stattin@gmail.com>
2015-02-03 17:06:13 +0100

    Added logo and started sorting out tags

--- public/js/app/randoom.js
+++ public/js/app/randoom.js
@@ -12,7 +12,17 @@ define([
 		var tableData = new TableCollection();
 		tableData.fetch({
 			success: function(payload) {
-				React.render(<TableList tables={payload.toJSON()} />, document.getElementById('tablesWindow'));
+				var data = payload.toJSON();
+
+				var tags = [];
+				data.forEach(function(d) {
+					d.tags.forEach(function(tag) {
+						tags.push(tag);
+					});
+				});
+				tags = _.uniq(tags);
+
+				React.render(<TableList tables={data} />, document.getElementById('tablesWindow'));
 				React.render(<Result />, document.getElementById('resultWindow'));
 			}
 		});
